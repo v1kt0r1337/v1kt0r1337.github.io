7c230f01eda91334e7ec10cffc18b79e03cb539d
v1kt0r1337 <setervang@hotmail.com>
2023-01-26 18:59:14 +0100

    change default mode to light mode

--- cv/after.js
+++ cv/after.js
@@ -1,5 +1,7 @@
-if (params.theme === "light") {
-  document.getElementById("logo").src = "assets/Dedica_Logo_RGB_Cropped.png";
+if (params.theme === "dark") {
+  document.getElementById("logo").src =
+    "assets/Dedica_Logo_White_CMYK_Cropped.png";
+} else {
   // // Turn profile image grey:
   // document.getElementById("pic").style.webkitFilter = "grayscale(100%)";
   // document.getElementById("pic").style.filter = "grayscale(100%)";
--- cv/index.js
+++ cv/index.js
@@ -1,10 +1,10 @@
 const params = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
 });
-if (params.theme === "light") {
+if (params.theme === "dark") {
   const r = document.querySelector(":root");
-  r.style.setProperty("--bg-color", "#fff");
-  r.style.setProperty("--primary-color", "black");
+  r.style.setProperty("--bg-color", "#282c34");
+  r.style.setProperty("--primary-color", "#fff");
   r.style.setProperty("--secondary-color", "#999");
-  r.style.setProperty("--info-color", "#999");
+  r.style.setProperty("--info-color", "#61afef");
 }
